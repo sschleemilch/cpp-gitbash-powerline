15ddc97e17f999f58ad62c8d297cca4eb65273d1
Sebastian Schleemilch <basti.schleemilch@onlinehome.de>
2019-04-17 13:24:31 +0200

    Prompt symbol working

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,32 @@ extern "C" {
     #include "git2.h"
 }
 #include "bash_color.hpp"
+#include "unicode_symbols.hpp"
 #include "help_functions.hpp"
 
+void printPromptSymbol(int lastCommandReturnCode) {
+    if (lastCommandReturnCode == 0) {
+        BashColor::print(UnicodeSymbols::getString(UnicodeSymbols::SYMBOL::TRIANGLE_RIGHT),
+            BashColor::COLOR::PINK,
+            BashColor::COLOR::TRANSPARENT,
+            false);
+    } else {
+        BashColor::print(UnicodeSymbols::getString(UnicodeSymbols::SYMBOL::TRIANGLE_RIGHT),
+            BashColor::COLOR::RED,
+            BashColor::COLOR::TRANSPARENT,
+            false);
+    }
+}
 
-int main() {
+int main(int argc, char** argv) {
     std::string cwd = std::getenv("PWD");
     std::string home = std::getenv("HOME");
 
     //git_repository *repo = NULL;
     //int error = git_repository_open(&repo, "/tmp/…");
     BashColor::print(getCWDString(cwd, home),
-        BashColor::COLOR::GREEN,
         BashColor::COLOR::BLACK,
+        BashColor::COLOR::BLUE,
         false);
+    printPromptSymbol(atoi(argv[1]));
 }
--- src/unicode_symbols.cpp
+++ src/unicode_symbols.cpp
@@ -6,41 +6,41 @@ UnicodeSymbols::~UnicodeSymbols() {}
 std::string UnicodeSymbols::getString(SYMBOL s) {
     switch (s) {
         case SYMBOL::CROSS:
-            return "\u2715";
+            return "\\u2715";
         case SYMBOL::ANCHOR:
-            return "\u2693";
+            return "\\u2693";
         case SYMBOL::LIGHTNING:
-            return "\u21AF";
+            return "\\u21AF";
         case SYMBOL::FLAG:
-            return "\u2691";
+            return "\\u2691";
         case SYMBOL::PENCIL:
-            return "\u270E";
+            return "\\u270E";
         case SYMBOL::PLUS:
-            return "\u002B";
+            return "\\u002B";
         case SYMBOL::DELTA:
-            return "\u0394";
+            return "\\u0394";
         case SYMBOL::ARROW_UP_DOWN:
-            return "\u21C5";
+            return "\\u21C5";
         case SYMBOL::CHECK:
-            return "\u2713";
+            return "\\u2713";
         case SYMBOL::SUN:
-            return "\u2600";
+            return "\\u2600";
         case SYMBOL::HEART:
-            return "\u2665";
+            return "\\u2665";
         case SYMBOL::DANGER:
-            return "\u26A0";
+            return "\\u26A0";
         case SYMBOL::ARROW_RIGHT:
-            return "\u2023";
+            return "\\u2023";
         case SYMBOL::ARROW_UP:
-            return "\u2191";
+            return "\\u2191";
         case SYMBOL::ARROW_DOWN:
-            return "\u2193";
+            return "\\u2193";
         case SYMBOL::MERGE:
-            return "\u26D5";
+            return "\\u26D5";
         case SYMBOL::ARROW_RIGHT_2:
-            return "\u21A6";
+            return "\\u21A6";
         case SYMBOL::TRIANGLE_RIGHT:
-            return "\u22B3";
+            return "\\u22B3";
     }
     return std::string();
 }
